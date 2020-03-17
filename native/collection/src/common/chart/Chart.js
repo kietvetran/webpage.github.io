6c1ab3778f7b693414f46d1be1282498be87cb1a
Kiet Ve Tran <kietvetran@gmail.com>
2020-03-17 09:36:09 +0100

    Update native chart by drawing bar with path instead of rec.

--- native/collection/src/common/chart/Chart.js
+++ native/collection/src/common/chart/Chart.js
@@ -54,7 +54,7 @@ const ChartGraph = ({data, animate}) => {
   } else if ( data.type === 'line-cirle' ) {
     graph = <Circle id={data.id} cx={data.cx} cy={data.cy} r={data.radius} fill={data.color} style={data.style}>
     </Circle>;
-  } else if ( data.type === 'pie' || data.type === 'progress' || data.type === 'path' ) {
+  } else if ( data.type === 'pie' || data.type === 'progress' || data.type === 'bar-path' || data.type === 'path' ) {
     graph = <Path id={data.id} style={data.style} d={data.path}>
     </Path>
   } else if ( data.type === 'text' && data.text ) {
@@ -507,6 +507,53 @@ export default class Chart extends React.Component {
   _initGraphBarInfo( state, info, multiple ){
     info.width  = state.axis.x.max / info.list.length;
     info.list.forEach( (data, i) => {
+      data.type      = 'bar-path';
+      data.percent   = data.value / info.highest;
+      data.width     = info.width - (state.barSpace * 2);
+      data.height    = state.axis.y.max * data.percent;
+      data.x         = (info.width * i) + state.barSpace + state.padding;
+      data.y         = state.axis.y.max + state.padding;
+      data.center    = [data.x + (data.width/2), data.y - (data.height/2)];
+
+      if ( multiple && multiple.count > 1 && typeof(multiple.index) === 'number' ) {
+        data.width  = data.width / multiple.count;
+        data.x      = data.x + (multiple.index * data.width);
+      }
+
+      data.duration  = (state.duration / 1000)+'s';
+      data.color     = data.color || state.color.list[info.color++];
+      data.style     = {
+        'fill': data.color,
+        'stroke': state.color.default,
+        'strokeWidth': 1
+      };      
+
+      /*
+      data.animateAttributeName = 'd';
+      data.animateFrom = [
+        'M',
+        [data.x, data.y].join(','),
+        [data.x, data.y].join(','),
+        [data.x+data.width, data.y].join(','),
+        [data.x+data.width, data.y].join(',')
+      ].join(' ');
+      */
+
+      data.path = [
+        'M',
+        [data.x, data.y].join(','),
+        [data.x, data.y-data.height].join(','),
+        [data.x+data.width, data.y-data.height].join(','),
+        [data.x+data.width, data.y].join(',')
+      ].join(' ');
+
+    });
+  }
+
+  /*
+  _initGraphBarInfo( state, info, multiple ){
+    info.width  = state.axis.x.max / info.list.length;
+    info.list.forEach( (data, i) => {
       data.type      = 'bar';
       data.percent   = data.value / info.highest;
       data.width     = info.width - (state.barSpace * 2);
@@ -525,6 +572,7 @@ export default class Chart extends React.Component {
       }
     });
   }
+  */
 
   /****************************************************************************
   ****************************************************************************/
