3a5067965aaeade511812ccedf10691223b82eb1
Kiet Ve Tran <Kiet.Ve.Tran@Eika.no>
2020-05-24 18:37:39 +0200

    update developement.

--- developement/src/store/configureStore.js
+++ developement/src/store/configureStore.js
@@ -2,14 +2,13 @@ import { routerMiddleware } from 'react-router-redux';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import createSagaMiddleware from 'redux-saga';
+//import createSagaMiddleware from 'redux-saga';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers/index';
-import * as sagas from '../sagas';
+//import * as sagas from '../sagas';
 
 export default function configureStore(initialState) {
-  const sagaMiddleware = createSagaMiddleware();
-
+  //const sagaMiddleware = createSagaMiddleware();
   const store = createStore(
     rootReducer,
     initialState,
@@ -17,7 +16,7 @@ export default function configureStore(initialState) {
       applyMiddleware(
         reduxImmutableStateInvariant(),
         routerMiddleware(),
-        sagaMiddleware,
+        //sagaMiddleware,
         thunkMiddleware,
       ),
     ),
@@ -31,11 +30,13 @@ export default function configureStore(initialState) {
     });
   }
 
+  /*
   const initSagas = (saga) => {
     Object.values(sagas).forEach(saga.run.bind(saga));
   };
 
   initSagas(sagaMiddleware);
+  */
 
   return store;
 }
